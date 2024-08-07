a7455f3a86b62e9188adcaca2f4ca217da63191e
Vlada <vlada.belayy@mail.com>
2024-08-07 21:03:47 +0300

    commit

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,17 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_one_penny() -> None:
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+
+
+def test_penny_and_nickel() -> None:
+    assert get_coin_combination(6) == [1, 1, 0, 0]
+
+
+def test_pennies_nickel_and_dime() -> None:
+    assert get_coin_combination(17) == [2, 1, 1, 0]
+
+
+def test_two_quarters() -> None:
+    assert get_coin_combination(50) == [0, 0, 0, 2]
